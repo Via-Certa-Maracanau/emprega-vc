477fdfa7ddb6cd202e1ccfe72baade1a9fb61b6a
Via Certa Maracanau <viacertamaracanau@gmail.com>
2024-06-28 20:06:52 -0300

    alterações dos estilos no na pagina inicial e do curriculo

--- assets/js/lista.js
+++ assets/js/lista.js
@@ -1,7 +1,7 @@
 iconList = document.getElementsByClassName("iconList")
 
 for(i = 0; i < iconList.length; i++) {
-    iconList[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M480-360 280-560h400L480-360Z"/></svg>`
+    iconList[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#696969"><path d="M480-360 280-560h400L480-360Z"/></svg>`
 }
 
 function verificarLista(id, i) {
@@ -9,11 +9,11 @@ function verificarLista(id, i) {
 
     if(lista.style.display == "unset") {
         fechar(lista)
-        iconList[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M480-360 280-560h400L480-360Z"/></svg>`
+        iconList[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#696969"><path d="M480-360 280-560h400L480-360Z"/></svg>`
     }
     else {
         abri(lista)
-        iconList[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="m280-400 200-200 200 200H280Z"/></svg>`
+        iconList[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#696969"><path d="m280-400 200-200 200 200H280Z"/></svg>`
     }
 }
 
